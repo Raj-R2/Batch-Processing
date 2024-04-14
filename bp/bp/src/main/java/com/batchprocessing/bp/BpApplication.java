1ec5195cd196cb6f11e0a45388b9e90815e2acb5
Raj-R2 <rajsavaliya2713@gmail.com>
2024-04-14 10:39:24 +0530

    adding initial files

---
+++ bp/bp/src/main/java/com/batchprocessing/bp/BpApplication.java
@@ -0,0 +1,13 @@
+package com.batchprocessing.bp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpApplication.class, args);
+	}
+
+}
